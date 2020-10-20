e3df5683b594a5d773af3fa2101fd59d273b6ab2
Laurens <8708858+Razpudding@users.noreply.github.com>
2020-10-20 19:53:28 +0200

    Expanded example somewhat

--- index.js
+++ index.js
@@ -22,11 +22,26 @@ d3.csv('data/Survey_Information_Design_clean-parsed.json', (data) => {
 //Load that file in html BEFORE you load this file so it's available
 const surveyAnswers = data
 console.log(surveyAnswers)
+
+//Let's see what's inside one of these objects
+console.log(surveyAnswers[0])
+
+//Get some super specific thing out of them
+//
+
+//Let's list important subjects
 let belangrijksteOnderwerpen = []
 for (answer of surveyAnswers){
 	console.log(answer)
 	belangrijksteOnderwerpen.push(answer.belangrijksteOnderwerpVanDezeTijd)
 }
 
+//Let's change some of this data to have the right datatype
+
+
 console.log(belangrijksteOnderwerpen)
-//map, filter chainen? string manips chainen?
+//Unieke values listen
+//map, filter chainen?
+// string manip opschonen?
+// eye color voorbeeld laten zien https://vizhub.com/Razpudding/43af65cee12c440c8992b9bfadd81eab?edit=files&file=index.js
+// Vaknaam string manip laten zien> https://github.com/cmda-tt/course-20-21/blob/master/examples/string-manipulation.md
